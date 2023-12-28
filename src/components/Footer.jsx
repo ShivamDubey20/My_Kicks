00badf9c42812e0c9d4e394221984a6567a11b08
ShivamDubey20 <shivam.dubey2020@vitbhopal.ac.in>
2023-12-28 20:19:10 +0530

    commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import {AiFillGithub} from 'react-icons/ai'
+
+const Footer = () => {
+  
+  return (
+   <>
+      <footer className='bg-theme mt-10 pt-2 pb-5 text-slate-200'>
+          <div className='mt-5 text-center'>
+            <p className='flex justify-center text-sm md:text-center'>Copyright<sup className='text-base font-bold'>&copy;</sup> All Reserved Rights <span className='font-semibold'> Shivam Dubey </span></p>
+          </div>
+      </footer>
+   </>
+  )
+}
+
+export default Footer
